a74da07293ddd39fa32d5e9786e7d9dadc708bca
rstubryan <restubumirr@gmail.com>
2025-10-03 23:14:07 +0700

    feat: implement authentication pages and middleware for user sessions

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,13 @@
+import { signup } from "../server/actions";
+
+export default function LoginPage() {
+  return (
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={signup}>Sign up</button>
+    </form>
+  );
+}
